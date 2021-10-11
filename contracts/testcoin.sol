8266742aa058f4cc4b6fe5b41faee7cf237ab9f6
Eric-Chesnut <eric.t.chesnut@gmail.com>
2021-10-11 16:38:07 -0700

    started coin contract

---
+++ contracts/testcoin.sol
@@ -0,0 +1,7 @@
+// Specifies the version of Solidity, using semantic versioning.
+// Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
+pragma solidity ^0.5.2;
+
+contract TestCoin {
+
+}
