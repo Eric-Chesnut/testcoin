62e79d2af0d9c15026674e9ac3cf86d3ecf31b19
Eric-Chesnut <eric.t.chesnut@gmail.com>
2021-10-26 23:28:51 -0700

    tokenURI override and added convert to base64 library
    overwrote the tokenURI function to output svg, will be used to store data that varies by individual

--- contracts/testcoin.sol
+++ contracts/testcoin.sol
@@ -40,6 +40,7 @@ contract TestCoin is ERC721Enumerable, ReentrancyGuard, Ownable {
         return uint256(keccak256(abi.encodePacked(input)));
     }
 	
+	//uses a number, tokenId or # is recomended, and a string to generate a random number
 	function getrandom(uint256 tokenId, string memory keyPrefix) internal view returns (uint256) {
         uint256 rand = random(string(abi.encodePacked(keyPrefix, toString(tokenId))));
 		return rand;
@@ -67,4 +68,107 @@ contract TestCoin is ERC721Enumerable, ReentrancyGuard, Ownable {
         return string(buffer);
     }
 	
+	function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        string[9] memory parts;
+        parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 100 50"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="20" y="15" class="base">WORD...?</text>';
+
+        parts[1] = 'A';
+
+        parts[2] = '</text><text x="15" y="40" class="base">';
+
+        parts[3] = 'A';
+
+        parts[4] = '</text><text x="35" y="40" class="base">';
+
+        parts[5] = 'A';
+
+        parts[6] = '</text><text x="55" y="40" class="base">';
+
+        parts[7] = 'A';
+
+        parts[8] = '</text><text x="75" y="40" class="base">';
+
+
+        string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7], parts[8]));
+        
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "N #',
+                        toString(tokenId),
+                        '", "description": "N is just numbers.", "image": "data:image/svg+xml;base64,',
+                        Base64.encode(bytes(output)),
+                        '"}'
+                    )
+                )
+            )
+        );
+        output = string(abi.encodePacked("data:application/json;base64,", json));
+
+        return output;
+    }
+	
+}
+
+/// [MIT License]
+/// @title Base64
+/// @notice Provides a function for encoding some bytes in base64
+/// @author Brecht Devos <brecht@loopring.org>
+library Base64 {
+    bytes internal constant TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    /// @notice Encodes some bytes to the base64 representation
+    function encode(bytes memory data) internal pure returns (string memory) {
+        uint256 len = data.length;
+        if (len == 0) return "";
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((len + 2) / 3);
+
+        // Add some extra buffer at the end
+        bytes memory result = new bytes(encodedLen + 32);
+
+        bytes memory table = TABLE;
+
+        assembly {
+            let tablePtr := add(table, 1)
+            let resultPtr := add(result, 32)
+
+            for {
+                let i := 0
+            } lt(i, len) {
+
+            } {
+                i := add(i, 3)
+                let input := and(mload(add(data, i)), 0xffffff)
+
+                let out := mload(add(tablePtr, and(shr(18, input), 0x3F)))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(12, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(input, 0x3F))), 0xFF))
+                out := shl(224, out)
+
+                mstore(resultPtr, out)
+
+                resultPtr := add(resultPtr, 4)
+            }
+
+            switch mod(len, 3)
+            case 1 {
+                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
+            }
+            case 2 {
+                mstore(sub(resultPtr, 1), shl(248, 0x3d))
+            }
+
+            mstore(result, encodedLen)
+        }
+
+        return string(result);
+    }
 }
