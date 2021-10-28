a3971bfcdf12da2c3b7f0e890a1cede96b6a0e87
Eric-Chesnut <eric.t.chesnut@gmail.com>
2021-10-28 13:49:58 -0700

    turn off abi in console

--- contract-interact.js
+++ contract-interact.js
@@ -3,7 +3,7 @@ const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const contract = require("./build/contracts/TestCoin.json"); 
-console.log(JSON.stringify(contract.abi));
+//console.log(JSON.stringify(contract.abi));
 // Provider
 const alchemyProvider = new ethers.providers.AlchemyProvider(network="ropsten", API_KEY);
 // Signer
